ec69ea776537637bd60d3be93de601f656e8a07f
JamesCDavenport <112661163+JamesCDavenport@users.noreply.github.com>
2025-05-18 14:49:50 -0400

    Added audio sender and receiver

--- audio.py
+++ audio.py
@@ -3,18 +3,24 @@ import queue
 import json
 from vosk import Model, KaldiRecognizer
 
+import os
+import sys
+
+sys.stderr = open(os.devnull, 'w')
+
+
 def transcribe_from_mic_offline(model_path="vosk-model-small-en-us-0.15", duration=5, samplerate=16000):
     q = queue.Queue()
 
     def callback(indata, frames, time, status):
-        if status:
-            print(f"Status: {status}")
+        # if status:
+        #     print(f"Status: {status}")
         q.put(bytes(indata))
 
     model = Model(model_path)
     recognizer = KaldiRecognizer(model, samplerate)
 
-    print(f"Listening for {duration} seconds... Speak now.")
+    # print(f"Listening for {duration} seconds... Speak now.")
     with sd.RawInputStream(samplerate=samplerate, blocksize=8000, dtype='int16',
                            channels=1, callback=callback):
         for _ in range(int(samplerate / 8000 * duration)):
@@ -27,4 +33,8 @@ def transcribe_from_mic_offline(model_path="vosk-model-small-en-us-0.15", durati
     final_result = json.loads(recognizer.FinalResult())
     return final_result.get("text", "")
 
-print(transcribe_from_mic_offline())
+# print(transcribe_from_mic_offline())
+
+if __name__ == "__main__":
+    while True:
+        print(transcribe_from_mic_offline())
